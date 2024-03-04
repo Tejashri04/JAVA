247497cb1688d73ed26e28fe7847cfcbe92290da
Tejashri04 <114688886+Tejashri04@users.noreply.github.com>
2024-03-04 23:36:32 +0530

    Add files via upload

---
+++ Strings/concatenate.java
@@ -0,0 +1,8 @@
+public class concatenate{
+    public static void main(String args[]){
+        String firstName = "Tejashri";
+        String lastName = "Tisage";
+        String fullName = firstName +" "+ lastName ;
+        System.out.println(fullName);
+    }
+}
