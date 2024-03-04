bc2d6ab0269c4b3b3f47ecba5b60bb370729d9d4
Tejashri04 <114688886+Tejashri04@users.noreply.github.com>
2024-03-04 22:56:30 +0530

    Add files via upload

---
+++ Strings/declaration.java
@@ -0,0 +1,12 @@
+//JAVA Strings are IMMUTABLE 
+
+public class declaration{
+    public static void main(String args[]){
+
+        String str = "abcd";
+            
+            //or
+            
+        String str2 = new String("xyz");
+    }
+}
