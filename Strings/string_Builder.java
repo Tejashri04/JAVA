f1156b759b4756f1bcb8c08f8c6a3127e78a0a21
Tejashri04 <114688886+Tejashri04@users.noreply.github.com>
2024-03-09 18:36:51 +0530

    Add files via upload

---
+++ Strings/string_Builder.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class string_Builder{
+    public static void main(String args[]){
+        
+         StringBuilder sb = new StringBuilder("Hello");
+         System.out.println(sb); 
+
+        
+    }
+}
