2cc55e701cbfe1b8112d907cfcbdf65a5cdc3254
Tejashri04 <114688886+Tejashri04@users.noreply.github.com>
2024-03-11 21:08:02 +0530

    Add files via upload

---
+++ BIT MANIPULATION/bit_manipulation.java
@@ -0,0 +1,24 @@
+public class bit_manipulation{
+    public static void main(String args[]){
+
+        // Bit-wise AND
+        System.out.println(5 & 6); 
+
+        // Bit-wise OR
+        System.out.println(5 | 6);
+
+        // Bit-wise XOR
+        System.out.println(5 ^ 6);
+
+        // Binary One's Complement(NOT operator)
+        System.out.println(~5); 
+
+        // Binary Left Shift 
+        System.out.println(5 << 2);
+
+         // Binary Right Shift 
+        System.out.println(6 >> 1);
+
+
+    }
+}
