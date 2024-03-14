f5b93600cf2b843af76e8f04b31dc7af93d7a70b
Tejashri04 <114688886+Tejashri04@users.noreply.github.com>
2024-03-14 11:21:24 +0530

    Add files via upload

--- BIT MANIPULATION/operations.java
+++ BIT MANIPULATION/operations.java
@@ -21,13 +21,44 @@ public class operations{
     public static int clear_iBit(int n , int i){
         int bitMask = ~(1 << i);
         return n & bitMask;
+    } 
+
+    // update i^th bit
+    public static int update_iBit(int n , int i, int newBit){
+        if(newBit == 0){
+            return clear_iBit(n,i);
+        }
+        else{
+            return set_iBit(n,i);
+        }
+    } 
+
+    // clear i bits
+    public static int clear_iBits(int n , int i){
+        int bitMask = (~0) << i;
+        return n & bitMask;
+    } 
+
+    // clear bits in range
+    public static int clear_bitsInRange(int n , int i , int j){
+        int a = ((~0) << (j+1));
+        int b = (1 << i) - 1;
+        int bitMask = a | b ;
+        return n & bitMask;
     }
+
     public static void main(String args[]){
         
-        System.out.println(get_iBit(10,2));
+        System.out.println(get_iBit(15,2));
 
         System.out.println(set_iBit(10,2));
 
         System.out.println(clear_iBit(10,1));
+
+        System.out.println(update_iBit(10,2,1));
+
+        System.out.println(clear_iBits(15,2));
+
+        System.out.println(clear_bitsInRange(10,2,4));
     }
 }
