4fe5a1fbdfb091075ea3b6405e7d698e7d3cfa56
Tejashri04 <114688886+Tejashri04@users.noreply.github.com>
2024-03-15 11:17:47 +0530

    Add files via upload

---
+++ BIT MANIPULATION/addOne.java
@@ -0,0 +1,15 @@
+// Add 1 to an integer using bit manipulation
+
+public class addOne{
+    public static void main(String args[]){
+        int x = 6;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+        
+        x = -4 ;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+        
+        x = 0 ;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+
+    }
+}
