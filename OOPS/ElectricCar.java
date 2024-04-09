3cdff36ee901b399e20ec90baa0a249ec5a374d5
Tejashri04 <114688886+Tejashri04@users.noreply.github.com>
2024-04-09 09:25:05 +0530

    Add files via upload

---
+++ OOPS/ElectricCar.java
@@ -0,0 +1,25 @@
+
+class Automobile{
+    private String drive(){
+        return "Driving vehicle";
+    }
+} 
+
+class Car extends Automobile{
+    protected String drive(){
+        return "Driving car";
+    }
+} 
+
+public class ElectricCar extends Car{
+    public final String drive(){
+        return "Driving electric car";
+    } 
+
+    public static void main(String[] wheels){
+        final Car c = new ElectricCar();
+        System.out.println(c.drive());
+    }
+
+} 
+
