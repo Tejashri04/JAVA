88606bd7c26da58ff8dcfe9b94e2eed63f383e92
Tejashri04 <114688886+Tejashri04@users.noreply.github.com>
2024-06-13 11:53:15 +0530

    Add files via upload

---
+++ ArrayList/Define.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Define{
+    public static void main(String args[]){
+        
+        // ClassName objectName = new ClassName();
+
+        ArrayList< Integer >list = new ArrayList<>();
+
+        ArrayList< String >list2 = new ArrayList<>();
+    }
+}
